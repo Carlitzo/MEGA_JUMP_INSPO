7a76db9ffc26c10fccc6823ecb1f6e98ef13824e
Carlitzo <adrianscholl95@hotmail.com>
2026-03-11 16:30:44 +0100

    asd

--- server.js
+++ server.js
@@ -1,3 +1,17 @@
-import { serveDir } from "jsr:@std/http/file-server";
+import { serveDir } from "jsr:@std/http";
 
-Deno.serve((req) => serveDir(req, "./public/index.html"));
+// För loggning med stacktrace
+const originalLog = console.log;
+
+console.log = (...args) => {
+  const stack = new Error().stack?.split("\n")[2] || "Unknown location";
+  const location = stack.trim().replace(/^at\s+/, "");
+  originalLog(`[${location}]`, ...args);
+};
+
+Deno.serve((request) => {
+  return serveDir(request, {
+    fsRoot: ".",
+    quiet: true
+  });
+});
