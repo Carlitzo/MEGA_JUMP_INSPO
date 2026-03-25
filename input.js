3a59da9ca229c68dfd5747bd903b0212f3d5d6da
Carlitzo <adrianscholl95@hotmail.com>
2026-03-25 16:22:56 +0100

    touchpad test

--- input.js
+++ input.js
@@ -2,3 +2,17 @@ export const keys = {};
 
 window.addEventListener('keydown', (e) => keys[e.code] = true );
 window.addEventListener('keyup', (e) => keys[e.code] = false );
+
+window.addEventListener('touchstart', (e) => {
+        const touch = e.touches[0];
+        if (touch.clientX < window.innerWidth / 2) {
+            keys['ArrowLeft'] = true;
+        } else {
+            keys['ArrowRight'] = true;
+        }
+    });
+    
+    window.addEventListener('touchend', () => {
+        keys['ArrowLeft'] = false;
+        keys['ArrowRight'] = false;
+    });
